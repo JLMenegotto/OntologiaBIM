0e8f41763b7d420b7a9c902ead4cbd1b58f7685a
José Luis Menegotto <jlmenegotto@poli.ufrj.br>
2023-03-15 13:25:12 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 using VDS.RDF;
 using VDS.RDF.Ontology;
 using VDS.RDF.Parsing;
@@ -17,29 +16,28 @@ namespace OntoBIM
     {
         public static void Main(String[] args)
         {
-            MessageBox.Show("Inicia Leitura");
-            
-            string prefixos = "Prefixos\n";
-            string dataprop = "Propriedades de dados\n";
-            string objeprop = "Propriedades de objetos\n";
-            string classes  = "Classes\n";
+            string pastap   = "C:\\JLMenegotto\\Projetos\\OntoBIM\\";
+            string pastaa   = "C:\\JLMenegotto\\Aplicativos_BIM\\OntoBIM\\";
+            string meuuri   = "https://jlmenegotto.wixsite.com/jlmenegotto-bim#ambientes";
+            string prefixos = "Prefixos     \n";
+            string dataprop = "Prop dados   \n";
+            string objeprop = "Prop objetos \n";
+            string classes  = "Classes      \n";
             bool nodo       = false;
 
-            OntologyGraph     g = new OntologyGraph();
-
-            FileLoader.Load ( g , "C://JLMenegotto//Projetos//OntoBIM//OntoBIM_ambientes_001.ttl");
-            INode Inodo = g.GetUriNode(new Uri("https://jlmenegotto.wixsite.com/jlmenegotto-bim#ambientes"));
+            OntologyGraph o     = new OntologyGraph();  FileLoader.Load ( o , pastap +  "OntoBIM_ambientes_001.ttl");
+            INode         Inodo = o.GetUriNode (new Uri ( meuuri ));
             
-            IEnumerable<OntologyClass>    clas = g.OwlClasses;
-            IEnumerable<OntologyProperty> prop = g.OwlDatatypeProperties;
-            IEnumerable<OntologyProperty> data = g.OwlDatatypeProperties;
-            IEnumerable<string>           pref = g.NamespaceMap.Prefixes;
-                                          nodo = g.IsEmpty;
+            IEnumerable<OntologyClass>    clas = o.OwlClasses;
+            IEnumerable<OntologyProperty> prop = o.OwlDatatypeProperties;
+            IEnumerable<OntologyProperty> data = o.OwlDatatypeProperties;
+            IEnumerable<string>           pref = o.NamespaceMap.Prefixes;
+                                          nodo = o.IsEmpty;
 
             foreach (string           s in pref) { prefixos += "\n" + s;            }
             foreach (OntologyClass    c in clas) { classes  += "\n" + c.ToString(); }
-            foreach (OntologyProperty p in prop) { objeprop += "\n" + p.ToString(); }
-            foreach (OntologyProperty p in data) { dataprop += "\n" + p.ToString(); }
+            foreach (OntologyProperty p in prop) { objeprop += "\n" + p.ToString() + p.Domains.ToString(); }
+            foreach (OntologyProperty p in data) { dataprop += "\n" + p.ToString() + p.Domains.ToString(); }
 
             MessageBox.Show( nodo ? "VAZIO" : "CHEIO"            + "\n" +
                              Inodo.ToString()                    + "\n" + 
@@ -50,76 +48,50 @@ namespace OntoBIM
 
             //MessageBox.Show(prefixos);
             //MessageBox.Show(classes );
-            //MessageBox.Show(objeprop);
+              MessageBox.Show(objeprop);
             //MessageBox.Show(dataprop);
             //---------------------------------------------------------------------------------------
             //Exemplo de inferência de subclasses  Exemplo 1
             //---------------------------------------------------------------------------------------
-            string pasta1 = "C:\\JLMenegotto\\Aplicativos_BIM\\OntoBIM\\";
-            string resultadoSemi = "\n";
-            string resultadoInfe = "\n";
-
-            //Carregar dados e esquemas 
-            Graph dados1 = new Graph(); FileLoader.Load(dados1 , pasta1 + "Os_individuos.ttl");
-            Graph esque1 = new Graph(); FileLoader.Load(esque1 , pasta1 + "Os_esquemas.ttl" );
-
-            IUriNode clase1 = dados1.CreateUriNode("ex:Car");
-            IUriNode tipos1 = dados1.CreateUriNode(new Uri(RdfSpecsHelper.RdfType));
-            foreach (Triple t in dados1.GetTriplesWithPredicateObject( tipos1 , clase1))
-            {
-                   resultadoSemi += t.Subject.ToString() + "\n";
-            }
-            MessageBox.Show( "\nSem Inferência 1: " + resultadoSemi + "\n" );
-
-            //Faz a inferência e retorna as subclasses de carros
-            StaticRdfsReasoner razona = new StaticRdfsReasoner();
-            razona.Initialise( esque1 );
-            razona.Apply     ( dados1 );
+            string   resultadoInfe = "\n";
+            string   classe        = "ex:Car";
+            Graph    dados1        = new Graph(); FileLoader.Load( dados1 , pastaa + "Os_individuos.ttl");
+            Graph    esque1        = new Graph(); FileLoader.Load( esque1 , pastaa + "Os_esquemas.ttl" );
+            IUriNode clase1        = dados1.CreateUriNode( classe );
+            IUriNode tipos1        = dados1.CreateUriNode(new Uri(RdfSpecsHelper.RdfType));
+            StaticRdfsReasoner razona1 = new StaticRdfsReasoner();
+            razona1.Initialise( esque1 );
+            razona1.Apply     ( dados1 );
             foreach (Triple t in dados1.GetTriplesWithPredicateObject( tipos1 , clase1))
             {
                      resultadoInfe += t.Subject.ToString() + "\n";  
             }
-
-            MessageBox.Show(
-                               "\nSem Inferência 1: " + resultadoSemi + "\n" +
-                               "\nCom Inferência 1: " + resultadoInfe
-                           );
-
-
+            MessageBox.Show( "\nCom Inferência 1: " + resultadoInfe );
             //---------------------------------------------------------------------------------------
             //Exemplo de inferência de subclasses  Exemplo 2 com ambientes fofu:
             //---------------------------------------------------------------------------------------
-            resultadoSemi   = "\n";
-            resultadoInfe   = "\n";
-            string   pasta2 = "C:\\JLMenegotto\\Projetos\\OntoBIM\\";
-            Graph    dados2 = new Graph(); FileLoader.Load(dados2, pasta2 + "OntoBIM_ambientes_Individuos.ttl");
-            Graph    esque2 = new Graph(); FileLoader.Load(esque2, pasta2 + "OntoBIM_ambientes_Esquema.ttl");
-            IUriNode clase2 = dados2.CreateUriNode("fofu:Ambiente");
-            IUriNode tipos2 = dados2.CreateUriNode(new Uri(RdfSpecsHelper.RdfType));
+            resultadoInfe = "\n";
+            classe        = "fofu:CorpoEdificado";
 
-            //Resultaso sim fazer inferência
-            foreach (Triple t in dados2.GetTriplesWithPredicateObject(tipos2, clase2))
-            {
-                     resultadoSemi += t.Subject.ToString() + "\n";
-            }
-
-            MessageBox.Show("\nSem Inferência 2: " + resultadoSemi + "\n");
-
-            //Faz a inferência e retorna as subclasses de carros
+            Graph              dados2  = new Graph(); FileLoader.Load(dados2 , pastap + "OntoBIM_ambientes_Individuos.ttl");
+            Graph              esque2  = new Graph(); FileLoader.Load(esque2 , pastap + "OntoBIM_ambientes_Esquema.ttl");
+            IUriNode           clase2  = dados2.CreateUriNode ( classe );
+            IUriNode           tipos2  = dados2.CreateUriNode (new Uri(RdfSpecsHelper.RdfType));
             StaticRdfsReasoner razona2 = new StaticRdfsReasoner();
+
             razona2.Initialise(esque2);
             razona2.Apply     (dados2);
 
-            //Resultados com inferência
             foreach (Triple t in dados2.GetTriplesWithPredicateObject(tipos2, clase2))
             {
-                resultadoInfe += t.Subject.ToString() + "\n";
+                     
+                      resultadoInfe += 
+                                       "Sujeito: "   + t.Subject.ToString()   + "\n" +
+                                       "Objeto: "    + t.Object.ToString()    + "\n" +
+                                       "Predicado: " + t.Predicate.ToString() + "\n" +
+                                                      (t.IsGroundTriple ? "Verdadeiro":"Falso") + "\n";
             }
-            MessageBox.Show(
-                               "\nSem Inferência 2: " + resultadoSemi + "\n" +
-                               "\nCom Inferência 2: " + resultadoInfe
-                           );
-
+            MessageBox.Show( "\nCom Inferência 2: " + resultadoInfe   );
         }
     }
 }
