e9667c4327c5dbd3ddb557189467b7061ec811c9
José Luis Menegotto <jlmenegotto@poli.ufrj.br>
2024-05-12 13:35:59 -0300

    Add files via upload

--- VX/Program.cs
+++ VX/Program.cs
@@ -1,9 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
 using VDS.RDF;
 using VDS.RDF.Ontology;
 using VDS.RDF.Parsing;
+using VDS.RDF.Query;
 using VDS.RDF.Query.Inference;
+using VDS.RDF.Writing;
+using VDS.RDF.Writing.Formatting;
 
 namespace OntoBIM
 {
@@ -11,72 +15,80 @@ namespace OntoBIM
     {
         public static void Main(String[] args)
         {
-            Mensagem ( "\nEsta é uma aplicação que usa ontologias ttl. Aguarde 2 segundos..." , 2000 ); 
+               Mensagem ( "\nEsta é uma aplicação que usa ontologias ttl. Aguarde 1 segundo..." , 1000 );
 
-			string pastap  = "C:\\ABNT_Normas\\ABNT\\Onto_Projeto_Arquitetura\\";
-            string arquttl = pastap + "Arquitetura.ttl";
-            string meuuri  = "https://jlmenegotto.wixsite.com/jlmenegotto-bim#Elementos.Arquitetura";
-            //bool   nodo  = false;
+  			   string dir1 = "C:\\JLMenegotto\\Projetos\\OBIM\\";
+			   string dir2 = "C:\\ABNT_Normas\\ABNT\\Onto_Projeto_Arquitetura\\";
 
-            string prefixos = "\n------------------\nPREFIXOS        \n------------------\n";
-            string classes  = "\n------------------\nCLASSES         \n------------------\n";
-            string objeprop = "\n------------------\nPROP. DE OBJETOS\n------------------\n";
-            string dataprop = "\n------------------\nPROP. DE DADOS  \n------------------\n";
-            string nodosont = "\n------------------\nNODOS           \n------------------\n";
-            string tripleso = "\n------------------\nTRIPLES         \n------------------\n";
+			   string esq      = dir1 + "Esquemas.ttl";
+			   string fat      = dir1 + "Fatos.ttl";
+               string arquttl  = dir2 + "Arquitetura.ttl";
+               string meuuri   = "https://jlmenegotto.wixsite.com/jlmenegotto-bim#Elementos.Arquitetura";
 
-            TurtleParser  tute  = new TurtleParser ( );
-			OntologyGraph onto  = new OntologyGraph( ); FileLoader.Load   ( onto , arquttl );
-            IGraph        graf  = new Graph();          graf.LoadFromFile ( arquttl , tute );
-            INode         Inodo = onto.GetUriNode ( new Uri ( meuuri ));
-            //nodo                = o.IsEmpty;
+               TurtleParser  tute  = new TurtleParser ( );
+			   OntologyGraph onto  = new OntologyGraph( ); FileLoader.Load   ( onto , arquttl );
+               IGraph        graf  = new Graph();          graf.LoadFromFile ( arquttl , tute );
+               INode         Inodo = onto.GetUriNode ( new Uri ( meuuri ));
 
-            IEnumerable<string>           pref = onto.NamespaceMap.Prefixes;
-            IEnumerable <OntologyClass>   clas = onto.OwlClasses;
-            IEnumerable<OntologyProperty> prop = onto.OwlObjectProperties;
-            IEnumerable<OntologyProperty> data = onto.OwlDatatypeProperties;
-            IEnumerable<INode>            nodo = onto.AllNodes;
-            IEnumerable<Triple>           trip = onto.Triples;
+               IEnumerable<string> pref = onto.NamespaceMap.Prefixes;
+                         
+               Novos_Nodos  ( dir1 , "Exemplo");
+			   Ver_Conteudo ( dir2 , "Arquitetura.ttl");
 
-            foreach (string pre in pref)          { prefixos += pref          + "\n"; }
-            foreach (OntologyClass    cl in clas) { classes  += cl.ToString() + "\n"; }
-            foreach (OntologyProperty op in prop) { objeprop += op.ToString() + "\n"; }
-            foreach (OntologyProperty dp in data) { dataprop += dp.ToString() + "\n"; }
-
-            foreach (INode nd in nodo) 
-            {         
-                     nodosont += "\n-----------" + 
-                                 "\nNodo =     " + nd.ToString() + 
-                                 "\nTipo =     " + nd.NodeType.ToString(); 
-            }                                   
-            foreach (Triple tr in trip)         
-            {        tripleso += "\n-----------" +
-                                 "\nSujeito  = " + tr.Subject.ToString() +
-                                 "\nPredica  = " + tr.Predicate.ToString() +
-                                 "\nObjeto   = " + tr.Object.ToString();
-            }
-
-            Mensagem ( classes  , 500 ); 
-            Mensagem ( objeprop , 500 ); 
-            Mensagem ( dataprop , 500 ); 
-            Mensagem ( nodosont , 500 );
-            Mensagem ( tripleso , 500 );
-
-			Selecionar ( "C:/JLMenegotto/Projetos/OBIM" );
-
-			//-----------------------------------------------------------------------------------
-			// Teste simple do reasoner
-			//-----------------------------------------------------------------------------------
-			string dir = "C:\\JLMenegotto\\Projetos\\OBIM\\";
-            string esq = dir + "Esquemas.ttl";
-            string fat = dir + "Fatos.ttl";
-
-			Razonador ( "1" , esq , fat , ":Vehicle");
-			Razonador ( "2" , esq , fat , ":Car");
-			Razonador ( "3" , esq , fat , ":SportsCar");
+			   Razonador    ( "1" , esq , fat , ":Vehicle");
+			   Razonador    ( "2" , esq , fat , ":Car");
+			   Razonador    ( "3" , esq , fat , ":SportsCar");
 		}
 
-        public static void Razonador  (string num, string arquesq, string arqufat, string classe)
+		public static List<ILiteralNode> Novos_Nodos ( string dir , string arq) 
+        {
+                      Graph              grafo  = new Graph();
+			          IUriNode           RDF    = grafo.CreateUriNode ( UriFactory.Create ( "http://www.dotnetrdf.org" ));
+                      IUriNode           dito   = grafo.CreateUriNode ( UriFactory.Create ( "https://jlmenegotto.wixsite.com/jlmenegotto-bim/obim"));
+                      //IUriNode         dito   = grafo.CreateUriNode ( UriFactory.Create ( "http://example.org/obim"  ));
+                      List<ILiteralNode> Lnodos = new List<ILiteralNode> { };
+                      List<string>       Ltexto = new List<string> { "Nodo.A ", "Nodo.B ", "Nodo.C ", "Nodo.D ", "Nodo.E ", "Nodo.F ", "Nodo.G ", "Nodo.H " };
+                      foreach (string txt in Ltexto)
+                      {
+		                        ILiteralNode litnod = grafo.CreateLiteralNode( txt , "pt" );
+				                grafo.Assert ( new Triple ( RDF , dito , litnod ) );
+			          }
+                  	  foreach (Triple tri in grafo.Triples)
+			          {
+				            	Mensagem(tri.ToString() , 5 );
+				      }
+			          RdfXmlWriter   EscreveXML = new RdfXmlWriter   ( );
+			          NTriplesWriter EscreveNT3 = new NTriplesWriter ( );
+			          EscreveXML.Save ( grafo , dir + arq + ".rdf");
+			          EscreveNT3.Save ( grafo , dir + arq + ".owl");
+		       return Lnodos;
+		}
+		public static void Ver_Conteudo ( string dir , string arq ) 
+		{
+			          OntologyGraph onto = new OntologyGraph();
+			          FileLoader.Load   (onto, dir + arq);
+                      string texto = "";
+                      //------------------------------------------------------------------------------------------
+			          List<OntologyClass>    clase = onto.OwlClasses.ToList( );
+                      List<OntologyProperty> propi = onto.OwlObjectProperties.ToList( );
+                      List<OntologyProperty> dados = onto.OwlDatatypeProperties.ToList( );
+			          List<INode>            nodos = onto.AllNodes.ToList( );
+		              List<Triple>           tripl = onto.Triples.ToList ( );
+
+			//------------------------------------------------------------------------------------------
+			foreach (OntologyClass    cla in clase) { texto += "\nClasse:  " + cla.ToString(); }
+            foreach (OntologyProperty pro in propi) { texto += "\nObjeto:  " + pro.ToString(); }
+			foreach (OntologyProperty dad in dados) { texto += "\nDado:    " + dad.ToString(); }
+			foreach (INode            nod in nodos) { texto += "\nNodo:    " + nod.ToString(); 
+		                                              texto += "\nTipo:    " + nod.NodeType.ToString();
+			}
+			foreach (Triple           tri in tripl) { texto += "\nSujeito: " + tri.Subject.ToString();
+	                                                  texto += "\nPredica: " + tri.Predicate.ToString();
+				                                      texto += "\nObjeto:  " + tri.Object.ToString();
+			}
+            Mensagem (texto , 1000);
+		}
+		public static void Razonador    ( string num , string arquesq, string arqufat, string classe)
         {
 			   Mensagem  ("\nINICIA RAZONADOR: " + num + "   Aguarde 2 segundos.", 2000);  
 
@@ -103,9 +115,9 @@ namespace OntoBIM
                }
 			   Mensagem ("\nRESULTADO RAZONADOR: " + num + inferido , 2000 );
 		}
-        public static void Selecionar (string pasta ) 
+        public static void Selecionar   ( string dir              ) 
         {
-               DirectoryInfo Dir = new DirectoryInfo( pasta );
+               DirectoryInfo Dir = new DirectoryInfo( dir );
                int           i   = 1;
 			   foreach (FileInfo f in Dir.GetFiles())
                {
@@ -121,12 +133,10 @@ namespace OntoBIM
 				        Mensagem ("\nArquivo selecionado = " + f.Name , 500);
                }          
         }
-        public static void Mensagem   ( string txt , int tempo)
+        public static void Mensagem     ( string txt , int tempo  ) 
         {
 			   Console.WriteLine ( txt   );
   			   Thread.Sleep      ( tempo );
 		}
-
-
 	}
 }
